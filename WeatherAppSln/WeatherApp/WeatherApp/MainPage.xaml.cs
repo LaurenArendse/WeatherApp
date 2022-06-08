438af865372d82a459d03c5685098a9fc4cad1e4
LaurenArendse <102674344+LaurenArendse@users.noreply.github.com>
2022-06-08 16:05:07 +0200

    Completed Weather App

---
+++ WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models.Service;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+       
+        public async Task<Location> GetLocation()
+        {
+            Location location = await Geolocation.GetLocationAsync();
+            if (location != null)
+            {
+                  return location;
+               
+            }
+
+            throw new Exception("Cannot get location");
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            Location loc = await GetLocation();
+            WeatherInfo info = await GetRemoteWeather(loc);
+            BindingContext = info;
+        }
+
+       
+
+        private async Task<WeatherInfo> GetRemoteWeather(Location loc)
+        {
+            double latitude = loc.Latitude; //latitude range = -90 to 90
+            double longitude = loc.Longitude; //longitude range = -180 to 180 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "Application/json");
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid=c6a49940e972b93975b65fad74fa1515";
+            string response = await client.GetStringAsync(url);
+
+            WeatherInfo info = JsonConvert.DeserializeObject<WeatherInfo>(response);
+
+            return info;
+        }
+       
+    }
+}
---
+++ WeatherAppSln/WeatherApp/WeatherApp/Models/WeatherInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+
+    public class WeatherInfo
+    {
+
+        public string Description { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string Pressure { get; set; }
+
+        public string Cloud {get; set; } //all cloud information
+
+        public string Gust { get; set; } //wind gust
+
+        public string Speed { get; set; } //speed for wind
+
+        public string Deg { get; set; } //degree for wind
+
+        public DateTime DateTime { get; set; } //current date and time
+    }
+}
